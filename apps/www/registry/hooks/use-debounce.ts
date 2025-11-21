439fa34f7911d3fd6f9564240ba99badb1992bea
Josh <jfarrin73@gmail.com>
2025-11-20 22:36:50 -0600

    use debounce

--- apps/www/registry/hooks/use-debounce.ts
+++ apps/www/registry/hooks/use-debounce.ts
@@ -1,12 +1,38 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
+/**
+ * Returns a debounced value that only updates after a specified delay.
+ * Useful for search inputs to prevent excessive API calls.
+ *
+ * @template T
+ * @param {T} value - The value to be debounced
+ * @param {number} [delay=500] - The delay in milliseconds
+ * @returns {T} The debounced value
+ *
+ * @example
+ * const [query, setQuery] = useState("")
+ * const debouncedQuery = useDebounce(query, 300)
+ *
+ * useEffect(() => {
+ *   // Triggers only when user stops typing for 300ms
+ *   searchApi(debouncedQuery)
+ * }, [debouncedQuery])
+ */
 export function useDebounce<T>(value: T, delay: number = 500): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay)
-    return () => clearTimeout(timer)
-  }, [value, delay])
+    // Set a timer to update the value after the delay
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
-  return debouncedValue
+    // Clean up the timer if value changes (resetting the clock)
+    // or if the component unmounts
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
 }
--- apps/www/src/mdx-components.tsx
+++ apps/www/src/mdx-components.tsx
@@ -1,15 +1,17 @@
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-  return {
-    ...defaultMdxComponents,
-    pre: ({ ref: _ref, ...props }) => (
-      <CodeBlock {...props}>
-        <Pre>{props.children}</Pre>
-      </CodeBlock>
-    ),
-    ...components,
-  };
+	return {
+		...defaultMdxComponents,
+		pre: ({ ref: _ref, ...props }) => (
+			<CodeBlock {...props}>
+				<Pre>{props.children}</Pre>
+			</CodeBlock>
+		),
+		...TabsComponents,
+		...components,
+	};
 }
